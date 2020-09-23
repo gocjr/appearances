50ba8f95c5753f2e95e5f906e1d658bab51d3cf6
Giovani O.C.Jr <giovaniocjr@gmail.com>
2020-09-23 00:22:37 -0300

    Controller
    Created the controller that will manage the data

---
+++ src/Controller/Admin/AppController.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Appearances\Controller\Admin;
+
+use App\Controller\AppController as BaseAppController;
+
+class AppController extends BaseAppController
+{
+}
---
+++ src/Controller/Admin/AppearancesController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Appearances\Controller\Admin;
+
+class AppearancesController extends AppController
+{
+
+    public function index()
+    {
+    }
+
+    public function add()
+    {
+    }
+
+    public function view($id = null)
+    {
+    }
+
+    public function edit($id = null)
+    {
+    }
+
+    public function delete($id = null)
+    {
+    }
+}
