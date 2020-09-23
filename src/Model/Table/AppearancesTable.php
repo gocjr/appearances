fd1401a04d265169be1a45014deb3efcb6c87665
Giovani O.C.Jr <giovaniocjr@gmail.com>
2020-09-23 01:41:37 -0300

    Create AppearancesTable.php

---
+++ src/Model/Table/AppearancesTable.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Appearances\Model\Table;
+
+use App\Model\Table\AppTable;
+
+class AppearancesTable extends AppTable
+{
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+        $this->setTable('appearances');
+        $this->setDisplayField('title');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+    }
+}
