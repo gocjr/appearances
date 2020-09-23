1288580e6e5e9565c9131444c4c702fa15b7ea23
Giovani O.C.Jr <giovaniocjr@gmail.com>
2020-09-23 06:49:08 -0300

    Updated install method

--- src/Controller/Admin/AppController.php
+++ src/Controller/Admin/AppController.php
@@ -11,33 +11,6 @@ class AppController extends BaseAppController
 {
     public function install(): void
     {
-        $this->disableAutoRender();
-        $controller = strtolower($this->request->getParam('controller'));
-        $schema = new TableSchema($controller);
-        $schema
-            ->addColumn('id', [
-                'type' => 'integer',
-                'length' => 11,
-                'null' => false,
-                'default' => null,
-            ])
-            ->addColumn('title', [
-                'type' => 'string',
-                'length' => 120,
-                'fixed' => true
-            ])
-            ->addColumn('slug', [
-                'type' => 'string',
-                'length' => 120,
-                'fixed' => true
-            ])
-            ->addConstraint('primary', [
-                'type' => 'primary',
-                'columns' => ['id']
-            ])
-            ->options([
-                'engine' => 'InnoDB',
-                'collate' => 'utf8_unicode_ci',
-            ]);
+      
     }
 }
--- src/Controller/Admin/AppearancesController.php
+++ src/Controller/Admin/AppearancesController.php
@@ -7,6 +7,38 @@ namespace Appearances\Controller\Admin;
 class AppearancesController extends AppController
 {
 
+    public function install(): void
+    {
+        $this->disableAutoRender();
+        $controller = strtolower($this->request->getParam('controller'));
+        $schema = new TableSchema($controller);
+        $schema
+            ->addColumn('id', [
+                'type' => 'integer',
+                'length' => 11,
+                'null' => false,
+                'default' => null,
+            ])
+            ->addColumn('title', [
+                'type' => 'string',
+                'length' => 120,
+                'fixed' => true
+            ])
+            ->addColumn('slug', [
+                'type' => 'string',
+                'length' => 120,
+                'fixed' => true
+            ])
+            ->addConstraint('primary', [
+                'type' => 'primary',
+                'columns' => ['id']
+            ])
+            ->options([
+                'engine' => 'InnoDB',
+                'collate' => 'utf8_unicode_ci',
+            ]);
+    }
+    
     public function index()
     {
     }
